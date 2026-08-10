95c2ba5a4911ee937d704761bbe19149a3f8405f
hobs <git@totalgood.com>
2026-08-10 12:55:47 -0700

    deploy

--- docs/deploy_gameshell_run_inside_vm.sh
+++ docs/deploy_gameshell_run_inside_vm.sh
@@ -1,10 +1,11 @@
 # DONTDO: more README-Browser.md 
 # more docs/browser-deployment.md
+sudo echo Warning: using sudo to configure this server -- $HOSTNAME || echo "This script requires sudo. Run 'sudo echo hello sudo' to log into sudoer user before running this script".
+sudo apt install --upgrade -y ttyd caddy
 cd $HOME
 git clone https://github.com/aboutalib1953/GameShell-Browser || echo "GameShell-Browser repo already cloned?"
 cd GameShell-Browser/
 # sudo apt update && sudo apt upgrade
-sudo apt install --upgrade -y ttyd
 docker build -t gameshell .
 # chmod +x deploy/*.sh  # DONTDO: PR corrects this
 # docker images | grep gameshell
@@ -18,6 +19,12 @@ sudo systemctl enable --now gameshell-janitor.service gameshell-ttyd.service
 
 systemctl --no-pager status gameshell-janitor.service 
 systemctl --no-pager status gameshell-ttyd.service 
+
+if [[ -n "$FQDN" ]] ; then
+   sed -i s%'0.0.0.0'%"$FQDN"%g deploy/Caddyfile
+fi
+sudo cp deploy/Caddyfile /etc/caddy/Caddyfile
+sudo systemctl reload caddy
 
 # TODO
 # configure Caddy/ufw/Docker to expose appropriate ports and allow http:/ to IP address?
